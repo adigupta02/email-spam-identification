dc1326e689164ff79e508eaab65a56ddfbc2ad51
Aditya Gupta <83332409+adigupta02@users.noreply.github.com>
2022-11-25 09:06:08 +0530

    updated

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
-# mkdir -p ~/.streamlit/
+mkdir -p ~/.streamlit/
 
-# echo "\
-# [server]\n\
-# port = $PORT\n\
-# enableCORS = false\n\
-# headless = true\n\
-# \n\
-# " > ~/.streamlit/config.toml
+echo "\
+[server]\n\
+port = $PORT\n\
+enableCORS = false\n\
+headless = true\n\
+\n\
+" > ~/.streamlit/config.toml
